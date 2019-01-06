cce596a6f4efce14684998fba6f8f345870727be
Abdi Muleta <abdimuleta@Abdis-MBP.home>
2019-01-06 20:05:24 +0200

    components

---
+++ JS-sandbox/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+  <ul>
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </ul>,
+  document.getElementById("root")
+) 
