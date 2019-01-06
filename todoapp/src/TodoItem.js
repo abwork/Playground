e6cba02231244a24e7960e1fd554a22e4438aedc
Abdi Muleta <abdimuleta@Abdis-MBP.home>
2019-01-06 20:43:54 +0200

    checkbox status

--- todoapp/src/TodoItem.js
+++ todoapp/src/TodoItem.js
@@ -3,7 +3,7 @@ import React from "react"
 function TodoItem(props) {
     return (
         <div className="todo-item">
-            <input type="checkbox" />
+            <input type="checkbox" checked={props.task.completed} />
             <p>{props.task.text}</p>
         </div>
     )
